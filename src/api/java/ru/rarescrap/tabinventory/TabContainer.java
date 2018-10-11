dfde94ad701fbde9b4fdaf64d631f22a4cf5338d
RareScrap <RareScrap@users.noreply.github.com>
2018-10-11 13:13:51 +0800

    Реализован удобный класс TabContainer, от которого могут наследоваться другие мододелы для быстрой и удобной поддержки инвентарей со вкладками.

---
+++ src/api/java/ru/rarescrap/tabinventory/TabContainer.java
@@ -0,0 +1,75 @@
+package ru.rarescrap.tabinventory;
+
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ICrafting;
+import ru.rarescrap.tabinventory.network.NetworkUtils;
+import ru.rarescrap.tabinventory.network.syns.TabInventorySync;
+import ru.rarescrap.tabinventory.utils.Utils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Базовая реализация контейнера с {@link TabInventory}'ями. Используйте его как суперклас для вашего контейнера
+ * для быстрой и удобной поддержки инвентарей со вкладкам.
+ *
+ * Если же вы не хотите наследовать от этого класса, то вот что вам вам нужно сделать, чтобы добавить пооддержку
+ * инвентарей со вкладкими в ваш контейнер:
+ * <ol>
+ *  <li>Реализуйте интерфейс {@link SupportTabs.Container}</li>
+ *  <li>В классе вашего контейнера создайте поле для объекта {@link TabInventorySync}. Этот объект будет заниматься
+ *  синхронизацией с контейнером на клиенте.</li>
+ *  <li>Оверрайдите {@link #addCraftingToCrafters(ICrafting)} и вызовите внутри него
+ *  {@link Utils#addCraftingToCrafters(Container,List, TabInventorySync, ICrafting)}. Это нужно для того, чтобы
+ *  изменить стандартную логику синхронизации контейнеров. Дело в том, что по дефолту Minecraft жестко привязывает
+ *  содержимое инвенторя к слотам. И если итемстаки в них не равны, на клиент отправляется пакет. Но так как
+ *  {@link TabInventory} не привязывает "жестко" свой контент к слотам, то и механизм синхронизации ему нужен другой.
+ *  Оверрайд и вызов метода {@link Utils#addCraftingToCrafters(Container,List, TabInventorySync, ICrafting)}
+ *  сделает так, чтобы как только к контейнеру подключался игрок, к нему будет отправлено состояние всех инвентарей
+ *  со вкладками, которые есть в контейнере. Такое поведение совпадает с дефолтным поведением игры: при добавлении
+ *  нового игрока - слать ему весь контент контейнера, который есть и на других клиентах.</li>
+ *  <li>Оверрайдите {@link #detectAndSendChanges()} и вызовите внутри него
+ *  {@link Utils#detectAndSendChanges(Container, List, TabInventorySync)}. Это заставит ваш контейнер корректно
+ *  высылать изменения на клиентские инвентари {@link TabInventory}: по одному пакету на каждое изменение. Именно так
+ *  делает сам Minecraft.</li>
+ * </ol>
+ *
+ * Вот и все! Как видите, либа старается следовать такому же подходу, как и ванильный код.
+ *
+ */
+public abstract class TabContainer extends Container
+    implements SupportTabs.Container {
+
+    public Map<String, TabInventory> tabInventories = new HashMap<String, TabInventory>();
+    protected TabInventorySync sync;
+
+    public TabContainer() {
+        this.sync = new TabInventorySync(NetworkUtils.getNetworkWrapper(), windowId);
+    }
+
+    @Override
+    public void addCraftingToCrafters(ICrafting p_75132_1_) {
+        Utils.addCraftingToCrafters(this, this.crafters, sync, p_75132_1_);
+    }
+
+    @Override
+    public void detectAndSendChanges() {
+        Utils.detectAndSendChanges(this, this.crafters, sync);
+    }
+
+    @Override
+    public TabInventory getTabInventory(String invName) {
+        return tabInventories.get(invName);
+    }
+
+    @Override
+    public Map<String, TabInventory> getTabInventories() {
+        return tabInventories;
+    }
+
+    @Override
+    public TabInventorySync getSync() {
+        return sync;
+    }
+}
