17998bdfeb8bfa9a7a52d8a3a23f4e2a673450fe
RareScrap <RareScrap@users.noreply.github.com>
2018-10-12 18:01:06 +0800

    Переосмыслен TabClickWindowMessage. По большому счету, он является оберткой над C0EPacketClickWindow. В связи с этим: 1) Поля message'а инкапсулированы внутрь C0EPacketClickWindow. Кроме поля tabName. 2) Теперь пакет пересылает свои данные через bytebuf не через записывания туда полей (как с ByteBufUtils), а сразу экспортируя целый пакет C0EPacketClickWindow 3) Добавлены комментарии к MessageHandler 4) Добавлены TODO'хи

--- src/api/java/ru/rarescrap/tabinventory/network/TabClickWindowMessage.java
+++ src/api/java/ru/rarescrap/tabinventory/network/TabClickWindowMessage.java
@@ -10,86 +10,81 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.client.C0EPacketClickWindow;
 import ru.rarescrap.tabinventory.SupportTabs;
 import ru.rarescrap.tabinventory.TabInventory;
 
+import java.io.IOException;
+
 /**
  * Сообщение, посылающееся на cервер при клике по {@link TabInventory}.
- * Является аналогом {@link C0EPacketClickWindow}, но для инвентарей типа {@link TabInventory}.
+ * Является оберткой над {@link C0EPacketClickWindow}, но для инвентарей типа {@link TabInventory}.
  */
 public class TabClickWindowMessage implements IMessage {
-    /** The id of the window which was clicked. 0 for player inventory. */
-    public int windowId;
-    /** Id of the clicked slot */
-    public int slotId;
-    /** Button used */
-    public int packedClickData;
-    /** A unique number for the action, used for transaction handling */
-    public short actionNumber;
-    /** The item stack present in the slot */
-    public ItemStack clickedItem;
-    /** Inventory operation mode */
-    public int mode;
+    /** Ванильный пакет, содержащий всю стандартную информацию о клике */
+    private C0EPacketClickWindow packetClickWindow;
     /** Имя вкладки, для которой был сделан клик */
-    String tabName;
+    private String tabName;
 
     // for reflection newInstance
     public TabClickWindowMessage() {}
 
+    // TODO: Javadoc к параметрам
     @SideOnly(Side.CLIENT)
-    public TabClickWindowMessage(int windowIdIn, int slotIdIn, int usedButtonIn, int mode, ItemStack clickedItemIn, short actionNumberIn, String tabname) {
-        this.windowId = windowIdIn;
-        this.slotId = slotIdIn;
-        this.packedClickData = usedButtonIn;
-        if (clickedItemIn != null)
-            this.clickedItem = clickedItemIn.copy(); // TODO: Зачем?
-        this.actionNumber = actionNumberIn;
-        this.mode = mode;
-        this.tabName = tabname;
+    public TabClickWindowMessage(int windowId, int slotId, int usedButton, int mode, ItemStack clickedItem, short actionNumber, String tabName) {
+        this.packetClickWindow =
+                new C0EPacketClickWindow(windowId, slotId, usedButton, mode, clickedItem, actionNumber);
+
+        this.tabName = tabName;
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-       windowId = ByteBufUtils.readVarInt(buf, 4);
-       slotId = ByteBufUtils.readVarInt(buf, 4);
-       packedClickData = ByteBufUtils.readVarInt(buf, 4);
-       clickedItem = ByteBufUtils.readItemStack(buf);
-       actionNumber = (short) ByteBufUtils.readVarShort(buf);
-       mode = ByteBufUtils.readVarInt(buf, 4);
        tabName = ByteBufUtils.readUTF8String(buf); // TODO: Может мне не нужно посылать имя инвенаря? Его же ведь можно достать из слота по индексу и прокастить
+
+        // Лучше пересылать весь пакет сразу, чем его поля. Для этого есть очень удобные методы.
+        packetClickWindow = new C0EPacketClickWindow();
+        try {
+            packetClickWindow.readPacketData(new PacketBuffer(buf.copy()));
+        } catch (IOException e) {
+            // Не представляю ситуации, когда это может случится.
+            Exception cause = new Exception("Can't read packet data. Check client constructor arguments", e);
+            e.initCause(cause).printStackTrace();
+            // TODO: Делать ли "throw e;"? Нужно ли крашить игру?
+        }
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
-        ByteBufUtils.writeVarInt(buf, windowId, 4);
-        ByteBufUtils.writeVarInt(buf, slotId, 4);
-        ByteBufUtils.writeVarInt(buf, packedClickData, 4);
-        ByteBufUtils.writeItemStack(buf, clickedItem);
-        ByteBufUtils.writeVarShort(buf, actionNumber);
-        ByteBufUtils.writeVarInt(buf, mode, 4);
-        ByteBufUtils.writeUTF8String(buf, tabName);
+        ByteBufUtils.writeUTF8String(buf, tabName); // Записываем название вкладки, для которой совершается клик
+
+        try {
+            packetClickWindow.writePacketData(new PacketBuffer(buf)); // И сам пакет со сведениями о клике
+        } catch (IOException e) {
+            // Не представляю ситуации, когда это может случится.
+            Exception cause = new Exception("Can't write packet data. Check client constructor arguments", e);
+            e.initCause(cause).printStackTrace();
+            // TODO: Делать ли "throw e;"? Нужно ли крашить игру?
+        }
     }
 
     public static class MessageHandler implements IMessageHandler<TabClickWindowMessage, IMessage> {
         @Override
-        public IMessage onMessage(TabClickWindowMessage message, MessageContext ctx) {
-
+        public IMessage onMessage(TabClickWindowMessage m, MessageContext ctx) {
             EntityPlayerMP entityPlayerMP = ctx.getServerHandler().playerEntity;
-            if (message.windowId == entityPlayerMP.openContainer.windowId) {
-                SupportTabs.Container container = (SupportTabs.Container) ctx.getServerHandler().playerEntity.openContainer; // TODO: намеренно не делаю проверку т.к. мне интеренсо возможен ли тут краш?
-                String invName = ((Container) container).getSlot(message.slotId).inventory.getInventoryName();
-                container.getTabInventory(invName).setCurrentTab(message.tabName); // TODO: Это нормально вообще? Или юзать TabInventory#setInventorySlotContents(int, ItemStack, String)?
 
-                C0EPacketClickWindow packet = new C0EPacketClickWindow(
-                        entityPlayerMP.openContainer.windowId,
-                        message.slotId,
-                        message.packedClickData,
-                        message.mode,
-                        message.clickedItem,
-                        message.actionNumber
-                );
-                ctx.getServerHandler().processClickWindow(packet);
+            if (m.packetClickWindow.func_149548_c() == entityPlayerMP.openContainer.windowId) {
+                // Получаем контейнер с поддежкой вкладок
+                SupportTabs.Container container = (SupportTabs.Container) ctx.getServerHandler().playerEntity.openContainer; // TODO: намеренно не делаю проверку т.к. мне интеренсо возможен ли тут краш?
+                // Получаем из контейнера слот по ID, чтобы потом узнать имя связанного с ним инвентаря
+                String invName = ((Container) container).getSlot(m.packetClickWindow.func_149544_d()).inventory.getInventoryName();
+                // И по этому имени достаем нужный TabInventory и устанавливаем ему
+                // вкладку, в которой был сделан клик
+                container.getTabInventory(invName).setCurrentTab(m.tabName); // TODO: Это нормально вообще? Или юзать TabInventory#setInventorySlotContents(int, ItemStack, String)?
+
+                // А дальше ванильный пакет все сделает за нас
+                ctx.getServerHandler().processClickWindow(m.packetClickWindow);
             }
 
             return null;
