9b990d27f06a4d7e1998a5870b203033297e9f13
RareScrap <RareScrap@users.noreply.github.com>
2018-10-12 18:00:09 +0800

    * В качестве родительствого класса теперь используется TabContainer, который озволит быстро добавить поддержку TabInventory'ей в конейнер * Добавлены TODO'хи с описанием возможных багов и крашей * Добавлены комментарии * Добавлено стартовое заполнения вкладок для наглядности либы * Выяснено, что onContainerClosed - выполняется и на клиенте, и на сервере. В связи с этим, данные в NBT сохраняются теперь только при закрытии серверного контейнера

--- src/main/java/ru/rarescrap/example/gui/BagContainer.java
+++ src/main/java/ru/rarescrap/example/gui/BagContainer.java
@@ -2,39 +2,42 @@ package ru.rarescrap.example.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import ru.rarescrap.tabinventory.TabContainer;
 import ru.rarescrap.tabinventory.TabHostInventory;
 import ru.rarescrap.tabinventory.TabInventory;
 
-public class BagContainer extends Container {
+public class BagContainer extends TabContainer {
     private EntityPlayer player;
     private ItemStack heldBag;
 
     public TabHostInventory tabHostInventory;
     public TabInventory tabInventory;
 
-    // TODO: ???
-    public BagContainer() {
-        tabInventory = new TabInventory("tab_inv", 36, player);
-        tabHostInventory = new TabHostInventory("tabs", 4, tabInventory);
-        //initTabs();
-    }
-
     public BagContainer(EntityPlayer player, ItemStack heldItem) {
         this.player = player;
         this.heldBag = heldItem;
 
-        tabInventory = new TabInventory("tab_inv", 36, player);
-        tabHostInventory = new TabHostInventory("tabs", 4, tabInventory);
+        // Инициализируем хост и вкладочный инвентарь к этому хосту
+        tabHostInventory = new TabHostInventory("tabs", 4);
+        tabInventory = new TabInventory("tab_inv", 36, player, tabHostInventory);
+
+        // Добавляем инвентарь со вкладками к синхронизации
+        tabInventories.put(tabInventory.getInventoryName(), tabInventory);
 
         if (heldItem.getTagCompound() == null) {
-            initTabs();
+            initTabs(); // Инициализируем контент, если при открытии nbt итема пусто
         } else {
-            tabHostInventory.readFromNBT(heldItem.getTagCompound());
+            // В противном случае - читаем уже существующий nbt
+            tabHostInventory.readFromNBT(heldItem.getTagCompound());// TODO: Предмет может быть переименован на наковальние и в этом случае он будет содержать nbt, которое не сможет обработаться в этой строке
             tabInventory.readFromNBT(heldItem.getTagCompound());
+        }
+
+        // Если на серверной стороне ...
+        if (!player.worldObj.isRemote) {
+            getSync().addSync(tabInventory); // то включим для инвентаря синхронизацию
         }
 
         addSlots();
@@ -46,6 +49,25 @@ public class BagContainer extends Container {
             tabHostInventory.setInventorySlotContents(1, new ItemStack(Items.wooden_pickaxe, 1));
             tabHostInventory.setInventorySlotContents(2, new ItemStack(Items.beef, 1));
             tabHostInventory.setInventorySlotContents(3, new ItemStack(Items.boat, 1));
+
+            // debug
+            // Базовое заполнение инвентаря для наглядности
+            ItemStack[] stacks = tabInventory.getTab(Items.record_11.getUnlocalizedName()).stacks;
+            for (int i = 0; i < stacks.length; i++) {
+                stacks[i] = new ItemStack(Items.iron_ingot, 1);
+            }
+            stacks = tabInventory.getTab(Items.wooden_pickaxe.getUnlocalizedName()).stacks;
+            for (int i = 0; i < stacks.length; i++) {
+                stacks[i] = new ItemStack(Items.gold_ingot, 1);
+            }
+            stacks = tabInventory.getTab(Items.beef.getUnlocalizedName()).stacks;
+            for (int i = 0; i < stacks.length; i++) {
+                stacks[i] = new ItemStack(Items.carrot, 1);
+            }
+            stacks = tabInventory.getTab(Items.boat.getUnlocalizedName()).stacks;
+            for (int i = 0; i < stacks.length; i++) {
+                stacks[i] = new ItemStack(Items.diamond_hoe, 1);
+            }
         }
     }
 
@@ -90,13 +112,14 @@ public class BagContainer extends Container {
         }
     }
 
-    /* Хотя этот метод и выполняется на стороне клиента, сохранять данные в nbt тут можно, т.к. итем
-     * будет синхронизирован автоматически */
     @Override
-    public void onContainerClosed(EntityPlayer p_75134_1_) {
-        heldBag.setTagCompound(new NBTTagCompound());
-        tabHostInventory.writeToNBT(heldBag.getTagCompound());
-        tabInventory.writeToNBT(heldBag.getTagCompound());
-        super.onContainerClosed(p_75134_1_);
+    public void onContainerClosed(EntityPlayer entityPlayer) {
+        // Сохраняем инвентари в NBT итема при закрытии контейнера
+        if (!entityPlayer.worldObj.isRemote) {
+            heldBag.setTagCompound(new NBTTagCompound()); // TODO: Предмет может быть переименован на наковальние и в этом случае потеряет свое имя после закрытия контейнера
+            tabHostInventory.writeToNBT(heldBag.getTagCompound());
+            tabInventory.writeToNBT(heldBag.getTagCompound());
+        }
+        super.onContainerClosed(entityPlayer);
     }
 }
